c4f5027a8880c884aeb28488df801c5e8231a693
ADITYA SINGH GANGWAR <103820233+gangwarady@users.noreply.github.com>
2026-08-08 15:27:38 +0900

    Step 4: decode_ids_to_tokens

--- model.py
+++ model.py
@@ -26,8 +26,9 @@ def encode_sentence_to_ids(sentence, token_to_id, unk_token='<unk>'):
     unk_id = token_to_id[unk_token]
     return [token_to_id.get(token, unk_id) for token in sentence.split()]
 
-# Step 4 - decode_ids_to_tokens (not yet solved)
-# TODO: implement
+# Step 4 - decode_ids_to_tokens
+def decode_ids_to_tokens(ids, id_to_token):
+    return [id_to_token[i] for i in ids]
 
 # Step 5 - pad_id_sequence (not yet solved)
 # TODO: implement
